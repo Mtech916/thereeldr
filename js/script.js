b4a82f8c01bdb48405e11cf6a64abb56270a81ab
Mtech916 <shredtechfit@gmail.com>
2023-11-17 17:25:52 -0800

    navbar + header

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// Replace Text in Header
+const checkReplace = document.querySelector('.replace-me');
+
+if (checkReplace != null) {
+  const replace = new ReplaceMe(checkReplace, {
+    animation: 'animated fadeIn',
+    speed: 2000,
+    separator: ',',
+    loopCount: 'infinite',
+    autoRun: true,
+  });
+}
+
+// User Scroll For Navbar
+function userScroll() {
+  const navbar = document.querySelector('.navbar');
+  const toTopBtn = document.querySelector('#to-top');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add('bg-light');
+      navbar.classList.add('border-bottom');
+      navbar.classList.add('border-secondary');
+      navbar.classList.add('navbar-sticky');
+      toTopBtn.classList.add('show');
+    } else {
+      navbar.classList.remove('bg-light');
+      navbar.classList.remove('border-bottom');
+      navbar.classList.remove('border-secondary');
+      navbar.classList.remove('navbar-sticky');
+      toTopBtn.classList.remove('show');
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', userScroll);
