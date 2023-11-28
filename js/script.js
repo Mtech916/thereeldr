512d1ae24082fb5f6d3d820c0a1aa26adb85a019
Mtech916 <shredtechfit@gmail.com>
2023-11-28 11:03:26 -0800

    Expertise and video Modal

--- js/script.js
+++ js/script.js
@@ -41,3 +41,28 @@ function scrollToTop() {
 // Event Listeners
 document.addEventListener('DOMContentLoaded', userScroll);
 document.querySelector('#to-top').addEventListener('click', scrollToTop);
+
+// Video Modal
+const videoBtn = document.querySelector('.video-btn');
+const videoModal = document.querySelector('#videoModal');
+const video = document.querySelector('#video');
+let videoSrc;
+
+if (videoBtn !== null) {
+  videoBtn.addEventListener('click', () => {
+    videoSrc = videoBtn.getAttribute('data-bs-src');
+  });
+}
+
+if (videoModal !== null) {
+  videoModal.addEventListener('shown.bs.modal', () => {
+    video.setAttribute(
+      'src',
+      videoSrc + '?autoplay=1;modestbranding=1;showInfo=0'
+    );
+  });
+
+  videoModal.addEventListener('hide.bs.modal', () => {
+    video.setAttribute('src', videoSrc);
+  });
+}
