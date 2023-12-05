84232f6b1733a256362939b677b459e1464ab395
Mtech916 <shredtechfit@gmail.com>
2023-12-04 17:45:17 -0800

    updated reviews, projects, footer

--- js/script.js
+++ js/script.js
@@ -66,3 +66,17 @@ if (videoModal !== null) {
     video.setAttribute('src', videoSrc);
   });
 }
+
+const panels = document.querySelectorAll('.panel');
+const removeActiveClasses = () => {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+};
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
